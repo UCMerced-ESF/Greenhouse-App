5a627eed3163cbb35431073e33c3d9283bc63658
Samuel408 <samuelsaldivar11@gmail.com>
2024-05-30 18:27:11 -0700

    made UI mobile friendly and updated navigation bar by adding uc merced logo

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import { ReactComponent as Logo } from './ucm_logo.svg';  // Adjust the path if the SVG is in a different directory
 const Nav = ({ onNavigationClick }) => {
     return (
            
 
              <nav id="top" style={navbarStyle}>
-                    <a style={activeLinkStyle} href="#top">UC Merced Greenhouse</a>
+                <a href="#top" style={ucmLogo}>
+                        <Logo/>
+                    </a>                
 
-                    <a  style={linkStyle} onClick={() => onNavigationClick('status')} href="#top">Status</a>
+                    <a style={linkStyle} onClick={() => onNavigationClick('status')} href="#top">Status</a>
                     <a style={linkStyle} onClick={() => onNavigationClick('requestForm')} href="#top">Request Form</a>
             </nav>
     
@@ -16,31 +18,23 @@ const Nav = ({ onNavigationClick }) => {
 
 // Define styles
 const navbarStyle = {
-    backgroundColor: 'white',
-    overflow: 'hidden',
+    justifycontents: 'center',
+    backgroundColor: '#003262',
+    height: '80px',  // Set the desired height of the navbar
+    padding: '0 20px',  // Add padding if needed
 };
 
-const activeLinkStyle = {
-    float: 'left',
-    display: 'block',
-    color: 'black',
-    textAlign: 'center',
-    padding: '14px 20px',
-    textDecoration: 'none',
-    backgroundColor: 'white',
-    fontSize: '20px',
-    fontWeight: 'bold',
+const ucmLogo = {
+    width: '50px',  // Adjust the width to make the logo smaller
+    height: 'auto', // Keep the aspect ratio
+    marginRight: '20px',  // Add some space after the logo
 };
 
 const linkStyle = {
-    float: 'left',
-    display: 'block',
-    color: 'black',
-    textAlign: 'center',
-    padding: '14px 20px',
+    color: 'white',  // Change color to white for better visibility
+    padding: '15px 20px',
     textDecoration: 'none',
+    fontSize: '18px',  // Example font size
 };
-
-
 
 export default Nav;
