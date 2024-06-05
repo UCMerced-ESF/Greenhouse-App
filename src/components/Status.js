d704ffcc30856ed5ffbcd7998796b034c6b6723b
Samuel408 <samuelsaldivar11@gmail.com>
2024-06-05 13:01:29 -0700

    cleaned up ui

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,29 +1,22 @@
 import React from 'react';
 
 const Status = () => {
-
-    return (
-     <div>
+  return (
+    <div>
       <h1>Current Greenhouse Status</h1>
-      <iframe src="https://merced-my.sharepoint.com/personal/ewaring_ucmerced_edu/_layouts/15/Doc.aspx?sourcedoc={2a8df419-b795-4af8-a72c-8ac6dff22508}&amp;action=embedview" width="100%" height="600px" frameborder="0">
+      <iframe 
+        src="https://merced-my.sharepoint.com/personal/ewaring_ucmerced_edu/_layouts/15/Doc.aspx?sourcedoc={2a8df419-b795-4af8-a72c-8ac6dff22508}&amp;action=embedview" 
+        width="100%" 
+        height="600px" 
+        frameBorder="0" 
+        title="Greenhouse Status">
         This is an embedded
-         <a target="_blank" href="https://office.com">Microsoft Office</a>
-          diagram, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.
-          </iframe>
+        <a target="_blank" rel="noopener noreferrer" href="https://office.com">Microsoft Office</a>
+        diagram, powered by 
+        <a target="_blank" rel="noopener noreferrer" href="https://office.com/webapps">Office</a>.
+      </iframe>
     </div>
-    );
+  );
 }
 
-// Define styles
-const containerStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '600px',
-};
-
-const imageStyle = {
-    width: '75%',
-    height: '100%',
-};
 export default Status;
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,33 +1,40 @@
 import React from 'react';
 import { ReactComponent as Logo } from './ucm_logo.svg';  // Adjust the path if the SVG is in a different directory
+
 const Nav = ({ onNavigationClick }) => {
     return (
-           
-
-             <nav id="top" style={navbarStyle}>
-                <a href="#top" style={ucmLogo}>
-                        <Logo/>
-                    </a>                
-
-                    <a style={linkStyle} onClick={() => onNavigationClick('status')} href="#top">Status</a>
-                    <a style={linkStyle} onClick={() => onNavigationClick('requestForm')} href="#top">Request Form</a>
-            </nav>
-    
+        <nav id="top" style={navbarStyle}>
+          
+            <div style={linksContainerStyle}>
+            <a href="#top" style={ucmLogo}>
+                <Logo />
+            </a>
+                <a style={linkStyle} onClick={() => onNavigationClick('status')} href="#top">Status</a>
+                <a style={linkStyle} onClick={() => onNavigationClick('requestForm')} href="#top">Request </a>
+            </div>
+        </nav>
     );
-}
+};
 
 // Define styles
 const navbarStyle = {
-    justifycontents: 'center',
+    display: 'flex',
+    alignItems: 'left',
     backgroundColor: '#003262',
     height: '80px',  // Set the desired height of the navbar
     padding: '0 20px',  // Add padding if needed
 };
 
 const ucmLogo = {
-    width: '50px',  // Adjust the width to make the logo smaller
+    width: '10%',  // Adjust the width to make the logo smaller
     height: 'auto', // Keep the aspect ratio
-    marginRight: '20px',  // Add some space after the logo
+    marginRight: '10%',  // Add some space after the logo
+};
+
+const linksContainerStyle = {
+    display: 'flex',
+    alignItems: 'left',
+    // marginLeft: 'auto',  // Pushes the links to the far right
 };
 
 const linkStyle = {
